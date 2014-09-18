523c599cd03c19e555f43197b625fd3e74df06dd
kingnot <kingnot0524@gmail.com>
2014-09-18 14:15:33 -0400

    Add some comments

--- traffic_lights_system_4_way_/traffic_lights_system_4_way_.ino
+++ traffic_lights_system_4_way_/traffic_lights_system_4_way_.ino
@@ -9,56 +9,58 @@
 void setup() 
 {  //start of setup
   pinMode(buzzer,OUTPUT);//Set Pin Mode as output
-  pinMode(NSgreenLed,OUTPUT);//Set North/South ledPin as output 
-  pinMode(NSyellowLed,OUTPUT);//Set North/South ledPin as output 
-  pinMode(NSredLed,OUTPUT);//Set North/South ledPin as output  
-  pinMode(WEgreenLed, OUTPUT);//Set West/East ledPin as output
-  pinMode(WEyellowLed, OUTPUT);
-  pinMode(WEredLed, OUTPUT);
+  pinMode(NSgreenLed,OUTPUT);//Set North/South green ledPin as output 
+  pinMode(NSyellowLed,OUTPUT);//Set North/South yellowledPin as output 
+  pinMode(NSredLed,OUTPUT);//Set North/South ledPin red as output  
+  pinMode(WEgreenLed, OUTPUT);//Set West/East green ledPin as output
+  pinMode(WEyellowLed, OUTPUT); //Set WE yellow ledPin as output
+  pinMode(WEredLed, OUTPUT); //Set WE red ledPin as Outpu
 }  //end of setup
 
 void loop() 
 {
    digitalWrite(NSredLed,HIGH); //Set N/S red LED to high
-   digitalWrite(WEredLed,HIGH);  
-   delay(2000); //Delay 8 second 
-   digitalWrite(WEredLed,LOW); //Set N/S red LED to low 
+   digitalWrite(WEredLed,HIGH);  //Set W/E red LED to high
+   delay(2000); //Delay 2 second 
+   digitalWrite(WEredLed,LOW); //Set W/E red LED to low 
    
-   digitalWrite(WEgreenLed,HIGH); //Set W/E red LED to high  
-   delay(10000); //Wait 1 second 
-   digitalWrite(WEgreenLed,LOW); //Set W/E red LED to low 
+   /*West-East directions start to go from this point*/
+   digitalWrite(WEgreenLed,HIGH); //Set W/E green LED to high  
+   delay(10000); //Wait 10 second 
+   digitalWrite(WEgreenLed,LOW); //Set W/E green LED to low 
    
-   digitalWrite(buzzer,HIGH); //Start making a sound
-   delay(10); //Wait 1ms 
+   digitalWrite(buzzer,HIGH); //Start making a sound to warn the light swich
+   delay(10); //Wait 10ms 
    digitalWrite(buzzer,LOW); //Stop making a sound
-   delay(400);
+   delay(400); //Wait 400ms
    digitalWrite(buzzer,HIGH); //Start making a sound
-   delay(10); //Wait 1ms 
+   delay(10); //Wait 10ms 
    digitalWrite(buzzer,LOW); //Stop making a sound
    
-   digitalWrite(WEyellowLed,HIGH); //Set W/Egreen LED to high  
-   delay(2000); //Delay 5 second 
-   digitalWrite(WEyellowLed,LOW); //Set W/E green LED to low 
+   digitalWrite(WEyellowLed,HIGH); //Set W/E yellow LED to high  
+   delay(2000); //Delay 2 second 
+   digitalWrite(WEyellowLed,LOW); //Set W/E yellow LED to low 
       
-   digitalWrite(WEredLed,HIGH); //Set W/Eyellow LED to high  
-   delay(2000); //Delay 1 second 
-   digitalWrite(NSredLed,LOW); //Set W/Eyellow LED to low   
+   digitalWrite(WEredLed,HIGH); //Set W/E red LED to high  
+   delay(2000); //Delay 2 second 
+   digitalWrite(NSredLed,LOW); //Set N/S red LED to low   
    
-   digitalWrite(NSgreenLed,HIGH); //Set W/E red LED to high  
-   delay(10000); //Delay 7 second 
-   digitalWrite(NSgreenLed,LOW); //Set W/E red LED to low 
+   /*North-South directions start to go from this point*/
+   digitalWrite(NSgreenLed,HIGH); //Set N/S green LED to high  
+   delay(10000); //Delay 10 second 
+   digitalWrite(NSgreenLed,LOW); //Set N/S green LED to low 
    
-   digitalWrite(buzzer,HIGH); //Start making a sound
-   delay(10); //Wait 1ms 
+   digitalWrite(buzzer,HIGH); //Start making a sound to warn the light switch
+   delay(10); //Wait 10ms 
    digitalWrite(buzzer,LOW); //Stop making a sound
-   delay(400);
+   delay(400); //Wait 400ms
    digitalWrite(buzzer,HIGH); //Start making a sound
-   delay(10); //Wait 1ms 
+   delay(10); //Wait 10ms 
    digitalWrite(buzzer,LOW); //Stop making a sound
    
-   digitalWrite(NSyellowLed,HIGH); //Set N/Sgreen LED to high  
-   delay(2000); //Delay 5 second 
-   digitalWrite(NSyellowLed,LOW); //Set N/S green LED to low 
+   digitalWrite(NSyellowLed,HIGH); //Set N/S yellow LED to high  
+   delay(2000); //Delay 2 second 
+   digitalWrite(NSyellowLed,LOW); //Set N/S yellow LED to low 
    
 }//end of loop
 
